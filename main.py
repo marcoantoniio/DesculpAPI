80d40b9da1edff8560d57e23e269aadcc2143c8d
Marco Antônio Valério <marcovalerio27j@gmail.com>
2026-01-20 16:55:49 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import api, translate
-
-
-NoAsAService_API = api.get_api_key("NAAS")
-excuse = api.get_excuse(NoAsAService_API)
-translated_excuse = translate.translate_text(excuse, target_language="pt")
-
-print(translated_excuse)
